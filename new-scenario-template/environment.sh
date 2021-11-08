fcc9df6fc7d2a7d9ec2a12b515c1e98188c521e0
jackyho522 <78006989+jackyho522@users.noreply.github.com>
2021-11-09 01:55:30 +0900

    Update environment.sh

--- new-scenario-template/environment.sh
+++ new-scenario-template/environment.sh
@@ -13,6 +13,6 @@ mkdir wordpress
 chmod -R 777 grafana
 ls
 pwd
-echo "This is automatically run when the scenario.\n
+echo "Different folders will be created.\n
 These folders are used for bind mounting\n
 All files will be run in e-commerce directory\n"
