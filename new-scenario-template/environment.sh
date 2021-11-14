dae8959be33bfdd18d1304ac57e77759cb6817c5
Jacky <78006989+jackyho522@users.noreply.github.com>
2021-11-15 01:54:29 +0900

    Update environment.sh

--- new-scenario-template/environment.sh
+++ new-scenario-template/environment.sh
@@ -4,13 +4,13 @@ mkdir e-commerce
 cd e-commerce
 mkdir cfg
 mkdir db_data
-touch ./cfg/my.cnf
-touch ./scripts/permission.sql
-mkdir grafana
-mkdir ./grafana/data
 mkdir plugins
 mkdir scripts
 mkdir wordpress
+mkdir grafana
+touch ./cfg/my.cnf
+mkdir ./grafana/data
+touch ./scripts/permission.sql
 chmod -R 777 grafana
 ls
 pwd
